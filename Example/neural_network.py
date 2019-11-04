32e3da384dd7bc7f4f44c3b7dae69b572db71988
rksmile <40001585+rksmile@users.noreply.github.com>
2019-11-04 20:41:40 +0100

    Update neural_network.py

--- Example/neural_network.py
+++ Example/neural_network.py
@@ -4,19 +4,9 @@ from data_reader import Reader
 reader = Reader()
 
 #Read CSV data
-reader.csv('data/color_data.csv')
+reader.csv('data.csv')
 
 #Format to X and y columns
 X, y = reader.X_y_split(reader.data, 4)
 
 X_train, X_test, y_train, y_test = reader.split_train_test(X, y)
-
-print(X_train)
-print(y_train)
-print(len(X_train))
-print(len(y_train))
-
-print(X_test)
-print(y_test)
-print(len(X_test))
-print(len(y_test))
